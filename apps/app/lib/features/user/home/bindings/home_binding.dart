1bad591da6a1b21bb17ae7f513b302581b8325f7
Mosaidur <asifbhuiyan135@gmail.com>
2026-08-28 19:00:00 +0600

    feat(home): connect HomeController with dynamic categories and latest videos API

--- apps/app/lib/features/user/home/bindings/home_binding.dart
+++ apps/app/lib/features/user/home/bindings/home_binding.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
-import '../controllers/home_controller.dart';
+import '../../../../data/repositories/category_repository.dart';
+import '../../../../data/repositories/video_repository.dart';
 import '../../auth/controllers/auth_controller.dart';
+import '../controllers/home_controller.dart';
 
 class HomeBinding extends Bindings {
   @override
@@ -8,6 +10,9 @@ class HomeBinding extends Bindings {
     if (!Get.isRegistered<AuthController>()) {
       Get.put<AuthController>(AuthController(), permanent: true);
     }
-    Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<HomeController>(() => HomeController(
+          videoRepository: Get.isRegistered<VideoRepository>() ? Get.find<VideoRepository>() : null,
+          categoryRepository: Get.isRegistered<CategoryRepository>() ? Get.find<CategoryRepository>() : null,
+        ));
   }
 }
